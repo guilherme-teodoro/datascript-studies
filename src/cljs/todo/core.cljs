ebdcb1d854b508d2de11abf2b29f2a528bf03124
Guilherme Teodoro <gmacedo1000@gmail.com>
2016-08-25 19:09:49 -0300

    Wipao

--- src/cljs/todo/core.cljs
+++ src/cljs/todo/core.cljs
@@ -1,44 +1,96 @@
 (ns todo.core
   (:require [reagent.core :as r]
             [reagent.ratom :refer-macros [reaction]]
-            [posh.reagent :refer [pull q posh!]]
+            [posh.reagent :refer [pull q posh!] :as p]
             [datascript.core :as d]))
+
+(def schema {:story/id {:db.unique :db.unique/identity}
+             :task/story {:db.valueType :db.type/long}})
 
 (def conn (d/create-conn))
 
 (posh! conn)
 
-(d/transact! conn [{:profile/name "guilherme"
-                    :profile/age 10
-                    :profile/sex :m}
-                   {:profile/name "maiscedo"
-                    :profile/age 40
-                    :profile/sex :f}
-                   {:profile/name "teodoro"
-                    :profile/age 40
-                    :profile/sex :m}])
+(d/transact! conn [{:story/id 1
+                    :story/title "Holerite"}
+                   {:story/id 2
+                    :story/title "Sistema de Ponto"}
+                   {:task/title "Selecao magica"
+                    :task/done? false
+                    :task/story 1}
+                   {:task/title "Reconhecimento magico"
+                    :task/done? false
+                    :task/story 1}
+                   {:task/title "Ponto via nfc"
+                    :task/done? true
+                    :task/story 2}
+                   {:task/title "Ponto via nfc"
+                    :task/done? false
+                    :task/story 2}
+                   {:task/title "Manda pros caras"
+                    :task/done? false
+                    :task/story 1}])
+
+(defn add-task [stories]
+  (let [value (r/atom nil)
+        story (r/atom nil)]
+    (fn [stories]
+      [:form {:on-submit (fn [e]
+                           (.preventDefault e)
+                           (d/transact! conn [{:task/title @value
+                                               :task/done? false
+                                               :task/story @story}]))}
+       [:h3 "adicionar de task"]
+       [:label
+        [:input {:type "text"
+                 :value @value
+                 :on-change #(reset! value (-> % .-target .-value))}]]
+
+       [:br]
+       (doall
+        (for [s stories]
+          ^{:key s}
+          [:label
+           [:input {:type "radio"
+                    :checked (= @story s)
+                    :on-change #(reset! story s)}]
+           (:story/title @(pull conn '[:story/title] s))]))
+       [:br]
+       [:label
+        [:button {:type "submit"} "Adicionar"]]])))
 
 (defn page []
-  (let [value (r/atom nil)]
-    (fn []
-      (let [profile-ids @(q '[:find ?id ?name
-                              :where [?id :profile/name ?name]] conn)]
-        [:div [:h2 "Welcome to todo"]
-         [:input {:type "text"
-                  :value @value
-                  :on-change (fn [e]
-                               (reset! value (-> e .-target .-value)))}]
-         [:button {:on-click #(d/transact! conn [{:profile/name @value
-                                                  :profile/age 4
-                                                  :profile/sex :m}])}
-          "Adicionar"]
-         (doall
-          (map
-           (fn [[id name]]
-             ^{:key id} [:li (str name " - "
-                                  (:profile/age @(pull conn '[:profile/age] id)))])
-           profile-ids))]))))
-
+  (let [stories @(q '[:find [?id ...]
+                      :where
+                      [?id :story/id]] conn)]
+    [:div
+     [:div
+      [:h1 "Tasks manheirinhas"]
+      [:hr]
+      (doall (for [story stories]
+               ^{:key story}
+               [:div
+                [:h2 (:story/title @(pull conn '[:story/title] story))
+                 (str " (" (count @(q '[:find [?task ...]
+                                        :in $ ?story
+                                        :where
+                                        [?task :task/story ?story]
+                                        [?task :task/done? true]] conn story)) ")")]
+                [:h3 ]
+                [:ul
+                 (doall
+                  (for [task @(q '[:find [?task ...]
+                                   :in $ ?story
+                                   :where
+                                   [?c    :story/id ?story]
+                                   [?task :task/story ?c]] conn story)]
+                    ^{:key task}
+                    [:li
+                     [:input {:checked (:task/done? @(pull conn '[:task/done?] task))
+                              :type "checkbox"
+                              :on-change #(p/transact! conn [[:db/add task :task/done? (not (:task/done? @(pull conn '[:task/done?] task)))]])}]
+                     [:span (:task/title @(pull conn '[:task/title] task))]]))]]))]
+     [add-task stories]]))
 
 (defn mount-root []
   (r/render [page] (.getElementById js/document "app")))
